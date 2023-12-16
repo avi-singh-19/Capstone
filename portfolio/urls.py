eb45c4fe1079784a1ed79e7aa9e2200aba93044f
avi_singh <avi.v.singh@gmail.com>
2023-12-16 16:00:17 +0000

    second commit - added validation for invalid ticker, nav bar updated and extra pages added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('take_in_stock_price/', views.take_in_stock_price, name='take_in_stock_price'),
+    path('professional_details/', views.professional_details, name='professional_details'),
+    path('personal_interests', views.personal_interests, name='personal_interests')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -7,6 +7,14 @@ def index(request):
     return render(request, "portfolio/index.html")
 
 
+def professional_details(request):
+    return render(request, "portfolio/professional_details.html")
+
+
+def personal_interests(request):
+    return render(request, "portfolio/personal_interests.html")
+
+
 def take_in_stock_price(request):
     form = StockTickerForm()  # Initialize form outside the conditional block
     if request.method == 'POST':
@@ -14,10 +22,20 @@ def take_in_stock_price(request):
         if form.is_valid():
             ticker = form.cleaned_data['ticker']
             stock_data = yf.Ticker(ticker)
-            last_price = stock_data.history(period='1d')['Close'].iloc[-1]
+            historical_data = stock_data.history(period='1d')
+
+            if not historical_data.empty and 'Close' in historical_data:
+                last_price = historical_data['Close'].iloc[-1]
+                message = None
+            else:
+                message = f"No historical data available for {ticker}, please try a valid ticker. Company names do " \
+                          f"not work here, sorry about that."
+                last_price = None
+
             return render(request, 'portfolio/stock_price.html', {
                 'ticker': ticker,
-                'last_price': last_price
+                'last_price': last_price,
+                'message': message
             })
 
     return render(request, 'portfolio/stock_form.html', {'form': form})
