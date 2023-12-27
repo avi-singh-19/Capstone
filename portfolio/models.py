c1b312cd11720e3187715d9bbff5cc61611f6145
avi_singh <avi.v.singh@gmail.com>
2023-12-27 18:04:43 +0000

    seventeenth commit - modified models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Stock(models.Model):
     ticker = models.CharField(max_length=10, unique=True)
-    company_name = models.CharField(max_length=100)
     date_searched = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.company_name} with ticker {self.ticker} last searched on {self.date_searched.strftime("%B %d %Y at %I:%M %p")}'
+        return f'{self.ticker} last searched on {self.date_searched.strftime("%B %d %Y at %I:%M %p")}'
